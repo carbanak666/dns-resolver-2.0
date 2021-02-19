074141d6712c44ed375e916452e002d4ecec8f46
carbanak666 <56926537+carbanak666@users.noreply.github.com>
2021-02-18 22:01:16 -0300

    Add files via upload

---
+++ dnsdart2.c
@@ -0,0 +1,46 @@
+/*
++=======================+
++CODIGO FEITO POR SOLDAT+
++=======================+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //biblioteca de familia de protocolo de internet
+#include <arpa/inet.h> //biblioteca operações de internet
+#include <netdb.h> //biblioteca para operações de redes
+#include <string.h> //para manipuar strings
+
+int main(int argc, char *argv[]) {
+
+    char *alvo;
+    alvo = argv[1];
+    struct hostent *host;
+
+    char txt[100];
+    char *resultado;
+
+    FILE *dart;
+    dart = fopen(argv[2], "r");
+    
+	if (argv[1] == NULL)
+	{
+	  //menu inicial
+	  printf("=================================================\n");
+	  printf("============= .. dns dart v2.0 .. ===============\n");
+	  printf("====== .. uso: ./dnsdart2 alvo.com dart.txt .. ==\n");
+	  printf("============== .. by soldat .. ==================\n");
+	  printf("=================================================\n");
+	  return 0;
+	}
+	while(fscanf(dart, "%s", &txt) != EOF) {
+          resultado = (char *) strcat(txt, alvo); //juntando os dois arquivos
+          host = gethostbyname(resultado);
+          if (host == NULL)
+          {
+          	continue;
+          }
+          printf("HOST ENCONTRADO ===> %s IP ====> %s\n", resultado, inet_ntoa(*((struct in_addr *)host->h_addr)));
+	}
+
+}
